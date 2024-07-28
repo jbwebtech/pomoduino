2104524902a7c15326073543b7ee72e83407b63b
Justin Bianchi <justin.bianchi@gulfstream.com>
2024-07-27 21:33:08 -0400

    Simple sine wave HSV rainbow animation

--- src/pomoduino/pomoduino.ino
+++ src/pomoduino/pomoduino.ino
@@ -10,22 +10,67 @@
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
+const uint8_t sineMatrix[360]={
+  0,   0,   0,   0,   0,   1,   1,   2, 
+  2,   3,   4,   5,   6,   7,   8,   9, 
+ 11,  12,  13,  15,  17,  18,  20,  22, 
+ 24,  26,  28,  30,  32,  35,  37,  39, 
+ 42,  44,  47,  49,  52,  55,  58,  60, 
+ 63,  66,  69,  72,  75,  78,  81,  85, 
+ 88,  91,  94,  97, 101, 104, 107, 111, 
+114, 117, 121, 124, 127, 131, 134, 137, 
+141, 144, 147, 150, 154, 157, 160, 163, 
+167, 170, 173, 176, 179, 182, 185, 188, 
+191, 194, 197, 200, 202, 205, 208, 210, 
+213, 215, 217, 220, 222, 224, 226, 229, 
+231, 232, 234, 236, 238, 239, 241, 242, 
+244, 245, 246, 248, 249, 250, 251, 251, 
+252, 253, 253, 254, 254, 255, 255, 255, 
+255, 255, 255, 255, 254, 254, 253, 253, 
+252, 251, 251, 250, 249, 248, 246, 245, 
+244, 242, 241, 239, 238, 236, 234, 232, 
+231, 229, 226, 224, 222, 220, 217, 215, 
+213, 210, 208, 205, 202, 200, 197, 194, 
+191, 188, 185, 182, 179, 176, 173, 170, 
+167, 163, 160, 157, 154, 150, 147, 144, 
+141, 137, 134, 131, 127, 124, 121, 117, 
+114, 111, 107, 104, 101,  97,  94,  91, 
+ 88,  85,  81,  78,  75,  72,  69,  66, 
+ 63,  60,  58,  55,  52,  49,  47,  44, 
+ 42,  39,  37,  35,  32,  30,  28,  26, 
+ 24,  22,  20,  18,  17,  15,  13,  12, 
+ 11,   9,   8,   7,   6,   5,   4,   3, 
+  2,   2,   1,   1,   0,   0,   0,   0
+};
+
 void setup() {
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.begin();           // INITIALIZE NeoPixel strip object
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
-  // Fill along the length of the strip in various colors...
-  colorWipe(strip, strip.Color(255,   0,   0)     , 50); // Red
-  colorWipe(strip, strip.Color(  0, 255,   0)     , 50); // Green
-  colorWipe(strip, strip.Color(  0,   0, 255)     , 50); // Blue
-  colorWipe(strip, strip.Color(  0,   0,   0, 255), 50); // True white (not RGB white)
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  int w = 0;
 
-  whiteOverRainbow(strip, 75, 5);
+  // Sine wave rainbow.
+  //  Source: https://www.instructables.com/How-to-Make-Proper-Rainbow-and-Random-Colors-With-/
+  for (int angle = 0; angle < 360; angle++) {
+    r = sineMatrix[(angle+120) % 360];
+    g = sineMatrix[angle];
+    b = sineMatrix[(angle+240) % 360];
 
-  pulseWhite(strip, 5);
+    setAllStripColor(r, g, b);
+    delay(500);
+  }
+}
 
-  rainbowFade2White(strip, 2, 5, 3);
+void setAllStripColor(int r, int g, int b) {
+  const uint32_t color = strip.Color(r, g, b);
+  for (int p = 0; p < LED_COUNT; p++) {
+    strip.setPixelColor(p, color);
+  }
+  strip.show();
 }
